63e9bd261b0a4eaee16e7a723a0d9008d8d74a1a
Avrumi Friedman <alex+6@lakewoodprogramming.com>
2018-05-10 21:49:59 -0400

    Initial commit

---
+++ AjaxDemo.LinqToSqlData/PersonRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AjaxDemo.LinqToSqlData
+{
+    public class PersonRepository
+    {
+        private string _connectionString;
+
+        public PersonRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<Person> GetPeople()
+        {
+            using (var context = new PeopleDataContext(_connectionString))
+            {
+                return context.Persons.ToList();
+            }
+        }
+
+        public void AddPerson(Person person)
+        {
+            using (var context = new PeopleDataContext(_connectionString))
+            {
+                context.Persons.InsertOnSubmit(person);
+                context.SubmitChanges();
+            }
+        }
+
+        public void Update(Person person)
+        {
+            using (var context = new PeopleDataContext(_connectionString))
+            {
+                context.Persons.Attach(person);
+                context.Refresh(RefreshMode.KeepCurrentValues, person);
+                context.SubmitChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var context = new PeopleDataContext(_connectionString))
+            {
+                context.ExecuteCommand("DELETE FROM People WHERE Id = {0}", id);
+            }
+        }
+    }
+}
---
+++ AjaxPeople.Web/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+using AjaxDemo.LinqToSqlData;
+
+namespace AjaxPeople.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetPeople()
+        {
+            //Thread.Sleep(2000);
+            var db = new PersonRepository(Properties.Settings.Default.ConStr);
+            return Json(db.GetPeople(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Add(Person person)
+        {
+            var db = new PersonRepository(Properties.Settings.Default.ConStr);
+            db.AddPerson(person);
+            return Json(person);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Person person)
+        {
+            var db = new PersonRepository(Properties.Settings.Default.ConStr);
+            db.Update(person);
+            return Json(person);
+        }
+
+        [HttpPost]
+        public void Delete(int id)
+        {
+            //PersonDb db = new PersonDb(Properties.Settings.Default.ConStr);
+            //db.Delete(id);
+        }
+    }
+}
